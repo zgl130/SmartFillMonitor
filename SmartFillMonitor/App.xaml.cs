1938f39e7464a07c2bc0506a9923ed1841d1616f
@half-a-time <Administrator@BS7003282>
2026-09-04 20:15:20 +0800

    添加项目文件。

---
+++ SmartFillMonitor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SmartFillMonitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
