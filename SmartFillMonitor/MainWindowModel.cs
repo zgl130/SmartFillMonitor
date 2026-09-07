402a8374986524717011323c447251feb3fec8b2
@half-a-time <Administrator@BS7003282>
2026-09-07 16:08:21 +0800

    通过CommunityToolkit.Mvvm框架实现属性通知界面，实现时间刷新。

--- SmartFillMonitor/MainWindowModel.cs
+++ SmartFillMonitor/MainWindowModel.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace SmartFillMonitor
 {
-    public class MainWindowModel
+    public partial class MainWindowModel : ObservableObject
     {
+        private readonly DispatcherTimer _timer;
+        [ObservableProperty]
+        private string currentTime;//CommunityToolkit.Mvvm这个框架会自动生成一个CurrentTime，对应View界面绑定使用。
+
+        public MainWindowModel()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+
+        private void _timer_Tick(object? sender, EventArgs e)
+        {
+            CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
     }
 }
