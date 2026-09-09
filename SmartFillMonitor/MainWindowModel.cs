c692af3f4610bd026148495069921186551172d9
@half-a-time <Administrator@BS7003282>
2026-09-09 16:28:16 +0800

    完成导航界面切换功能，通过样式模版设置导航按钮的样式，通过框架实现绑定命令配合资源字典实现导航页面切换。

--- SmartFillMonitor/MainWindowModel.cs
+++ SmartFillMonitor/MainWindowModel.cs
@@ -1,4 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+using SmartFillMonitor.ViewModels;
+using SmartFillMonitor.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +12,20 @@ using System.Windows.Threading;
 
 namespace SmartFillMonitor
 {
+    //类必须设置为partial
     public partial class MainWindowModel : ObservableObject
     {
-        private readonly DispatcherTimer _timer;
         [ObservableProperty]
         private string currentTime;//CommunityToolkit.Mvvm这个框架会自动生成一个CurrentTime，对应View界面绑定使用。
+        private readonly DispatcherTimer _timer;
 
-        public MainWindowModel()
+        [ObservableProperty]
+        private object mainContent;
+        private readonly IServiceProvider _serviceProvider;//用于从IOC容器中获取ViewModel的单例
+
+        public MainWindowModel(IServiceProvider serviceProvider)
         {
+            this._serviceProvider = serviceProvider;
             _timer = new DispatcherTimer();
             _timer.Interval = TimeSpan.FromSeconds(1);
             _timer.Tick += _timer_Tick;
@@ -25,6 +35,44 @@ namespace SmartFillMonitor
         private void _timer_Tick(object? sender, EventArgs e)
         {
             CurrentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
         }
+
+        #region Navigation Command
+        //固定写法CommunityToolkit.Mvvm这个框架会自动生成一个NavgiateToDashBoardCommand，对应View界面绑定使用。
+        //[RelayCommand]
+        //private void NavgiateToDashBoard()
+        //{
+        //    MainContent = _serviceProvider.GetRequiredService<DashBoardViewModel>();
+        //}
+
+        [RelayCommand]
+        private void Navgiate(string? destination)
+        {
+            if (destination == null)
+                return;
+            switch (destination)
+            {
+                case "DashBoard":
+                    MainContent = _serviceProvider.GetRequiredService<DashBoardViewModel>();
+                    break;
+                case "DashQuery":
+                    MainContent = _serviceProvider.GetRequiredService<DashQueryViewModel>();
+                    break;
+                case "Logs":
+                    MainContent = _serviceProvider.GetRequiredService<LogsViewModel>();
+                    break;
+                case "Alarms":
+                    MainContent = _serviceProvider.GetRequiredService<AlarmsViewModel>();
+                    break;
+                case "Setting":
+                    MainContent = _serviceProvider.GetRequiredService<SettingViewModel>();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
