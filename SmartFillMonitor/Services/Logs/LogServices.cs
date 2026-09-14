067ec4ccd80e80f64e3da7fc0c4b44118e4eeab1
@半块时光 <Admin@DESKTOP-DQ2Q4VL>
2026-09-14 22:27:30 +0800

    重新上传SmartFillMonitor\Services\Log\LogService.cs类

---
+++ SmartFillMonitor/Services/Logs/LogServices.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using System;
+
+namespace SmartFillMonitor.Services.Logs
+{
+    public static class LogServices
+    {
+        public static void Info(string message) => Log.Information(message);
+
+        public static void Warn(string message) => Log.Warning(message);
+
+        public static void Debug(string message) => Log.Debug(message);
+
+        public static void Verbose(string message) => Log.Verbose(message);
+
+        public static void Fatal(string message) => Log.Fatal(message);
+
+        public static void Fatal(string message, Exception ex) => Log.Fatal(ex, message);
+        public static void Error(string message) => Log.Error(message);
+        public static void Error(string message, Exception ex) => Log.Error(message);
+    }
+}
