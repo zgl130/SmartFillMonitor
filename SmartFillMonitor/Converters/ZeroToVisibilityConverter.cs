11f596cb3827602e0385c8c8439a0ced1b0bd520
@half-a-time <Administrator@BS7003282>
2026-09-22 16:53:03 +0800

    AlarmView界面开发完成

---
+++ SmartFillMonitor/Converters/ZeroToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SmartFillMonitor.Converters
+{
+    public class ZeroToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int count)
+            {
+                return count == 0 ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SmartFillMonitor/ViewModels/AlarmsViewModel.cs
+++ SmartFillMonitor/ViewModels/AlarmsViewModel.cs
@@ -1,16 +1,42 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using SmartFillMonitor.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SmartFillMonitor.ViewModels
 {
-    public class AlarmsViewModel
+    public partial class AlarmsViewModel:ObservableObject
     {
         public AlarmsViewModel()
         {
                 
         }
+
+        public ObservableCollection<AlarmUiModel> ActiveAlarms { get; set; }
+        public ObservableCollection<AlarmUiModel> HistoryAlarms { get; set; }
+        [ObservableProperty]
+        private int activeAlarmCount;
+        [ObservableProperty]
+        private DateTime historyStartDate=DateTime.Today.AddDays(-1);
+        [ObservableProperty]
+        private DateTime historyEndDate=DateTime.Today;
+
+        [RelayCommand]
+        private async Task RefreshAsync()
+        {
+
+        }
+
+        [RelayCommand]
+        private async Task LoadHistoryAlarmsAsync()
+        {
+
+        }
+
     }
 }
